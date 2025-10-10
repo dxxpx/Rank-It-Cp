db32d1043b63c232eb5b96ab5f0161a2a03abc95
dxxpx <dxxpka.20@gmail.com>
2025-10-11 00:45:27 +0530

    Modifications made in App

--- lib/plugins/qr_code_scanner/lib/src/lifecycle_event_handler.dart
+++ lib/plugins/qr_code_scanner/lib/src/lifecycle_event_handler.dart
@@ -17,6 +17,9 @@ class LifecycleEventHandler extends WidgetsBindingObserver {
       case AppLifecycleState.inactive:
       case AppLifecycleState.paused:
       case AppLifecycleState.detached:
+      case AppLifecycleState.hidden:
+        // TODO: Handle this case.
+        throw UnimplementedError();
     }
   }
 }
